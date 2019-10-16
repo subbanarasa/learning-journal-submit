51c69acfdaba7677433dddaeec071681541a7ee5
subbanarasa.puttana <subbanarasa.puttana@cgi.com>
2019-10-16 10:28:15 +0530

    Completed Daily stock trade data

--- daily-stock-trade-data/src/main/java/com/kafka/producer/dailystocktradedata/DailyStockTradeDataApplication.java
+++ daily-stock-trade-data/src/main/java/com/kafka/producer/dailystocktradedata/DailyStockTradeDataApplication.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvParser;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import com.kafka.producer.dailystocktradedata.model.DailyStockTradeData;
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,11 +17,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.core.KafkaTemplate;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Iterator;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.stream.Stream;
@@ -30,8 +33,11 @@ public class DailyStockTradeDataApplication implements CommandLineRunner {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    @Value(value = "${files.directory:.}")
-    private String filesDirectory;
+    @Value(value = "${files.src.directory:.}")
+    private String srcFilesDirectory;
+
+    @Value(value = "${files.archive.directory:.}")
+    private String archiveFilesDirectory;
 
     @Value(value = "${kafka.topic}")
     private String kafkaTopic;
@@ -39,9 +45,9 @@ public class DailyStockTradeDataApplication implements CommandLineRunner {
     @Autowired
     private KafkaTemplate<String, DailyStockTradeData> kafkaTemplate;
 
-	public static void main(String[] args) {
-		SpringApplication.run(DailyStockTradeDataApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DailyStockTradeDataApplication.class, args);
+    }
 
     @Override
     public void run(String... args) throws Exception {
@@ -49,41 +55,55 @@ public class DailyStockTradeDataApplication implements CommandLineRunner {
             logger.info("No of threads command line argument is missing");
             return;
         }
-        logger.info("No of threads given in command line::"+args[0]);
+        logger.info("No of threads given in command line::" + args[0]);
         Integer noOfThreads = Integer.valueOf(args[0]);
+        if (noOfThreads <= 0) {
+            logger.error("No of threads argumnt should be greater than 0");
+            return;
+        }
 
         ExecutorService executorService = Executors.newFixedThreadPool(noOfThreads);
+        if (!new File(srcFilesDirectory).exists()) {
+            logger.error("Input source directory does not exists::" + srcFilesDirectory);
+            return;
+        }
+        Stream<Path> files = Files.list(Paths.get(srcFilesDirectory)).filter(Files::isRegularFile);
+        logger.info("Files available in input directory::" + srcFilesDirectory + " ||" + files.count());
+        File[] filesTmp = new File(srcFilesDirectory).listFiles();
 
-        Stream<Path> files = Files.list(Paths.get(filesDirectory)).filter(Files::isRegularFile);
-        logger.info("Files available in input directory::"+filesDirectory+" ||"+files);
-        files.forEach( path ->  {
-            logger.info("File name::"+path.getFileName());
-            executorService.execute(() ->{
-                CsvMapper csvMapper = new CsvMapper();
-                csvMapper.enable(CsvParser.Feature.IGNORE_TRAILING_UNMAPPABLE);
-                csvMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-                CsvSchema schema = CsvSchema.emptySchema().withHeader();
-                logger.info(schema.toString());
-                ObjectReader oReader = csvMapper.readerFor(DailyStockTradeData.class).with(schema);
-                try (Reader reader = new FileReader(path.toFile())) {
-                    MappingIterator<DailyStockTradeData> dailyStockTradeDataMappingIterator = oReader.readValues(reader);
-                    int count = 0;
-                    while (dailyStockTradeDataMappingIterator.hasNext()) {
-                        DailyStockTradeData dailyStockTradeData = dailyStockTradeDataMappingIterator.next();
-                        kafkaTemplate.send(kafkaTopic,dailyStockTradeData);
-                        count++;
+        for (int i = 0; i < filesTmp.length; i++) {
+            File file = filesTmp[i];
+            if (file.isFile()) {
+                logger.info("File name::" + file.getName());
+                executorService.execute(() -> {
+                    CsvMapper csvMapper = new CsvMapper();
+                    csvMapper.enable(CsvParser.Feature.IGNORE_TRAILING_UNMAPPABLE);
+                    csvMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+                    CsvSchema schema = CsvSchema.emptySchema().withHeader();
+                    logger.info(schema.toString());
+                    ObjectReader oReader = csvMapper.readerFor(DailyStockTradeData.class).with(schema);
+                    try (Reader reader = new FileReader(file)) {
+                        MappingIterator<DailyStockTradeData> dailyStockTradeDataMappingIterator = oReader.readValues(reader);
+                        int count = 0;
+                        while (dailyStockTradeDataMappingIterator.hasNext()) {
+                            DailyStockTradeData dailyStockTradeData = dailyStockTradeDataMappingIterator.next();
+                            kafkaTemplate.send(kafkaTopic, dailyStockTradeData);
+                            count++;
+                        }
+                        logger.info("Total records in the file::" + file.getName() + "||" + count);
+                        FileUtils.moveFileToDirectory(file, new File(archiveFilesDirectory), true);
+                    } catch (Exception e) {
+                        logger.error(e.toString());
                     }
-                    logger.info("Total records for the file::"+path.getFileName()+"||"+count);
-                }catch (Exception e){
-                    logger.error(e.toString());
-                }
 
-            });
-        });
+
+                });
+            }
+
+        }
 
         executorService.shutdown();
 
     }
-
 
 }
--- maximum-traded-value/src/main/java/com/kafka/streams/maximumtradedvalue/MaximumTradedValueApplication.java
+++ maximum-traded-value/src/main/java/com/kafka/streams/maximumtradedvalue/MaximumTradedValueApplication.java
@@ -1,13 +1,54 @@
 package com.kafka.streams.maximumtradedvalue;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kafka.streams.maximumtradedvalue.model.DailyStockTradeData;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.ForeachAction;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Serialized;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.binder.kafka.streams.annotations.KafkaStreamsProcessor;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.kafka.support.serializer.JsonSerde;
+import org.springframework.messaging.handler.annotation.SendTo;
+
+import java.util.Arrays;
+import java.util.Date;
 
 @SpringBootApplication
 public class MaximumTradedValueApplication {
-
+    private static final Logger logger = LoggerFactory.getLogger(MaximumTradedValueApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(MaximumTradedValueApplication.class, args);
 	}
+
+
+    @EnableBinding(KafkaStreamsProcessor.class)
+    public static class WordCountProcessorApplication {
+
+        public static final String INPUT_TOPIC = "input";
+        public static final String OUTPUT_TOPIC = "output";
+        public static final int WINDOW_SIZE_MS = 30000;
+
+        @StreamListener(Sink.INPUT)
+        @SendTo(OUTPUT_TOPIC)
+        public KStream<String, DailyStockTradeData> process(KStream<String, String> input) {
+            ObjectMapper mapper = new ObjectMapper();
+            Serde<DailyStockTradeData> stockDataSerde = new JsonSerde<>( DailyStockTradeData.class, mapper );
+            input.foreach((s, dailyStockTradeData) -> logger.info("Input record::"+dailyStockTradeData));
+             return input.flatMapValues(value -> Arrays.asList(mapper.convertValue(value,DailyStockTradeData.class)))
+                     .groupBy((key, value) -> value.getTimestamp())
+                     .count()
+                    .toStream()
+                    .map((key, value) -> new KeyValue<>(null,value[0]));
+        }
+    }
 
 }
